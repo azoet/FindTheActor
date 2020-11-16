23691882f7b12fb042e405840ebbd647b94a6fb4
Amir Zoet <amirzoet@outlook.com>
2020-11-16 20:12:38 +0100

    Improve bounding box fontsize for different sizes

--- App/Detection.py
+++ App/Detection.py
@@ -9,9 +9,22 @@ class Detection:
         faces = self.model.detect_faces(image)
         drawing = image.copy()
         boxes = []
+        height, width, _ = drawing.shape
         for i, face in enumerate(faces):
             x, y, width, height = box = face['box']
-            drawing = cv2.rectangle(drawing, (x, y), (x+width, y+height), (0, 0, 255), 5)
-            drawing = cv2.putText(drawing, "Box " + str(i+1), (x, y), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 0, 255), 10)
+            fontscale = self.get_fontscale("Box " + str(i+1), width)
+            thickness = int(fontscale)
+            drawing = cv2.rectangle(drawing, (x, y), (x+width, y+height), (0, 0, 255), thickness)
+            drawing = cv2.putText(drawing, "Box " + str(i+1), (x, y-5), cv2.FONT_HERSHEY_COMPLEX,
+                                  fontscale, (0, 0, 255), thickness)
             boxes.append(box)
         return drawing, boxes
+
+    def get_fontscale(self, text, width):
+        for scale in reversed(range(0, 60, 1)):
+            textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=scale / 10, thickness=1)
+            new_width = textSize[0][0]
+            if new_width <= width:
+                return scale / 10
+        return 1
+
